e5a7c31b443b0f362aec73bea0f457a5c53e87dd
Dueon Kim <dueonkim89@yahoo.com>
2019-07-20 13:07:20 -0700

    left and right pagination buttons and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from "react";
-import { Container, Button, Icon, Input } from "semantic-ui-react";
+import Header from "./Header";
 import "../App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Container textAlign="center" className="globalContainer">
-          <Button icon className="globalTrash">
-            <Icon name="trash" />
-          </Button>
-          <Input
-            icon="search"
-            iconPosition="left"
-            placeholder="Search mail"
-            className="searchBar"
-          />
-        </Container>
+        <Header />
       </div>
     );
   }
