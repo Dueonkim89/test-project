bbeb8651c79cef4183e51a1921c17f47a106fb5a
Dueon Kim <dueonkim89@yahoo.com>
2019-07-21 14:56:36 -0700

    connect components to store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,46 @@
 import React, { Component } from "react";
 import { Container, Button, Icon, Input } from "semantic-ui-react";
+import { connect } from "react-redux";
 
-function Header() {
-  return (
-    <Container textAlign="center" className="globalContainer">
-      <Button icon className="globalTrash">
-        <Icon name="trash" />
-      </Button>
-      <Input
-        icon="search"
-        iconPosition="left"
-        placeholder="Search mail"
-        className="searchBar"
-      />
-      <Button icon className="leftPagination">
-        <Icon name="angle left" />
-      </Button>
-      <Button icon className="rightPagination">
-        <Icon name="angle right" />
-      </Button>
-      <span>1 - 50 of 238</span>
-    </Container>
-  );
+class Header extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <Container textAlign="center" className="globalContainer">
+        <Button icon className="globalTrash">
+          <Icon name="trash" />
+        </Button>
+        <Input
+          icon="search"
+          iconPosition="left"
+          placeholder="Search mail"
+          className="searchBar"
+        />
+        <Button
+          icon
+          className="leftPagination"
+          disabled={this.props.startPagination === 1}
+        >
+          <Icon name="angle left" />
+        </Button>
+        <Button
+          icon
+          className="rightPagination"
+          disabled={this.props.endPagination === this.props.emails.length}
+        >
+          <Icon name="angle right" />
+        </Button>
+        <span>
+          {this.props.startPagination} - {this.props.endPagination} of{" "}
+          {this.props.emails.length}
+        </span>
+      </Container>
+    );
+  }
 }
 
-export default Header;
+const mapStateToProps = ({ startPagination, endPagination, emails }) => {
+  return { startPagination, endPagination, emails };
+};
+
+export default connect(mapStateToProps)(Header);
--- src/components/Primary.js
+++ src/components/Primary.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import { connect } from "react-redux";
+import Faker from "faker";
 
 class Primary extends Component {
   componentDidMount() {
     let { props } = this;
     props.selected(props.location.pathname);
+    let arr = [];
+    for (let i = 0; i < 5; i++) {
+      const user = {
+        name: Faker.name.findName(),
+        message: Faker.lorem.sentence(),
+        date: Faker.date.past()
+      };
+      arr.push(user);
+      console.log(user.date);
+    }
+    //use faker
+    //set up redux
   }
 
   render() {
